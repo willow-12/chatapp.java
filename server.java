775ace475f0130ff556e36caf6ece817e2c4bfaa
willow-12 <yosefayalew46@gmail.com>
2025-12-09 08:01:19 -0800

    Add basic server socket setups

--- server.java
+++ server.java
@@ -1,1 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server started...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
 
