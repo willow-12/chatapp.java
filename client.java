311d83ffdf4397abed966785b44a5d87cf75d6aa
willow-12 <yosefayalew46@gmail.com>
2025-12-09 12:46:16 -0800

    Add basic client connection

--- client.java
+++ client.java
@@ -1,1 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 5000);
+            System.out.println("Connected to server.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
 
