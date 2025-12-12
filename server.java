a1ebe0e2874d52ffd2107074cc424b0a02f9ee6f
willow-12 <yosefayalew46@gmail.com>
2025-12-12 04:16:26 -0800

    Update server.java

--- server.java
+++ server.java
@@ -5,10 +5,23 @@ public class server {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(5000);
-            System.out.println("Server started...");
+            System.out.println("Server started. Waiting for client...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected!");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+            String msg;
+            while ((msg = in.readLine()) != null) {
+                System.out.println("Client: " + msg);
+                out.println(console.readLine());
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 }
-
