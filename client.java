15887e94972c086cfc0fdc2b713e26f820278471
willow-12 <yosefayalew46@gmail.com>
2025-12-12 09:34:23 -0800

    Update client.java

--- client.java
+++ client.java
@@ -4,11 +4,29 @@ import java.net.*;
 public class client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 5000);
-            System.out.println("Connected to server.");
-        } catch (Exception e) {
-            System.out.println(e);
+            Socket socket = new Socket("localhost", 1234);
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            Thread readerThread = new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = in.readLine()) != null) {
+                        System.out.println(msg);
+                    }
+                } catch (Exception e) {
+                }
+            });
+            readerThread.start();
+
+            String msg;
+            while ((msg = keyboard.readLine()) != null) {
+                out.println(msg);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
-
